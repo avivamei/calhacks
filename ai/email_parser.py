9e95cb3a80edcceeb18e20210d64d0377062a9fe
xavajk <xjkuehn@gmail.com>
2024-10-19 09:11:30 -0700

    improve search prompts

--- ai/email_parser.py
+++ ai/email_parser.py
@@ -28,25 +28,21 @@ credentials = get_gmail_credentials(
 api_resource = build_resource_service(credentials=credentials)
 gmail_toolkit = GmailToolkit(api_resource=api_resource)
 gmail_tools = gmail_toolkit.get_tools()
-gmail_llm = ChatGroq(model="llama3-groq-8b-8192-tool-use-preview", temperature=0, max_retries=2).bind_tools(gmail_tools)
+query_llm = ChatGroq(model='llama-guard-3-8b', temperature=0)
+gmail_llm = ChatGroq(model="llama3-groq-70b-8192-tool-use-preview", temperature=0).bind_tools([gmail_tools[2]])
 
 serper_search = GoogleSerperAPIWrapper()
 search_tool = Tool(name='serper_search', func=serper_search.run, description='Useful for when you need to search for information on the internet.')
 
-response = gmail_llm.invoke([
-    ('system', BASE_SEARCH.format(status='new applications only', num_results='15')),
-    ('user', NEW_APPLICATIONS)
-])
+# search_query = query_llm.invoke('Given the following examples for search queries, formulate a search query to retrieve emails related to a new or in progress job application.\n\nThe Gmail query. Example filters include from:sender, to:recipient, subject:subject, -filtered_term, in:folder, is:important|read|starred, after:year/mo/date, before:year/mo/date, label:label_name “exact phrase”. Search newer/older than using d (day), m (month), and y (year): newer_than:2d, older_than:1y. Attachments with extension example: filename:pdf. Multiple term matching example: from:amy OR from:david.')
+# print(search_query)
+
+response = gmail_llm.invoke([BASE_SEARCH, NEW_APPLICATIONS.format(cutoff='1 year', max_results=15)])
 print(response.tool_calls)
 emails = []
 for tc in response.tool_calls:
     tool = list(filter(lambda x: x.name == tc['name'], gmail_tools))[0]
     tool_out = tool.invoke(tc)
-    print(tool_out)
-    tool_outputs = json.loads(tool_out.content)
-    emails.extend(tool_outputs)
-
-for e in emails:
-    e['body'] = e['body'].replace('\n', '').replace('\r', '')
-    pprint(e)
+    tool_out = json.loads(tool_out.content)
+    emails.extend(tool_out)
 print(f'Found {len(emails)} emails!')
--- ai/gmail.py
+++ ai/gmail.py
@@ -1,11 +1,23 @@
-from email_monitor import EmailMonitor
+# from email_monitor import EmailMonitor
 
-monitor = EmailMonitor(credentials_file="google-cloud/client_secret_275014340897-k0gsju7fh1giri9ilim7cpt001hdrn4d.apps.googleusercontent.com.json")
-email = monitor.search_mail(
-    query={ "from": "careerbrew@substack.com" },
-    wait_for_match=True,
-    unread=True,
-    labels=["INBOX", "UPDATES"],
-    delay=10
+# monitor = EmailMonitor(credentials_file="oauth/client_secret_163202554535-p6pfh8qs471c2b10tusiqr4p8gnn48qi.apps.googleusercontent.com.json")
+# email = monitor.search_mail(
+#     query={ "from": "notifications-noreply@linkedin.com" },
+#     wait_for_match=True,
+#     unread=False,
+#     labels=["INBOX"],
+#     delay=10
+# )
+# print(email)
+
+from langchain_google_community import GmailToolkit
+from langchain_google_community.gmail.utils import build_resource_service, get_gmail_credentials
+
+# setup Gmail toolkit
+credentials = get_gmail_credentials(
+    scopes=["https://mail.google.com/"],
+    client_secrets_file="oauth/client_secret_163202554535-p6pfh8qs471c2b10tusiqr4p8gnn48qi.apps.googleusercontent.com.json",
 )
-print(email)
+api_resource = build_resource_service(credentials=credentials)
+gmail_toolkit = GmailToolkit(api_resource=api_resource)
+gmail_tools = gmail_toolkit.get_tools()
