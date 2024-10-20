3e4acea1bb359a4ded05952fb4cd4d19d2ac7bdd
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2024-10-20 03:03:09 -0700

    dynamic charts

--- calhacks/views/work_mode_chart.py
+++ calhacks/views/work_mode_chart.py
@@ -1,27 +1,16 @@
 import reflex as rx
 from reflex.components.radix.themes.base import LiteralAccentColor
-
-
-def flag(initials: str) -> rx.Component:
-    return rx.image(
-        src=f"https://flag.vercel.app/s/{initials}.svg",
-        loading="lazy",
-        decoding="async",
-        width="24px",
-        height="auto",
-        border_radius="2px",
-    )
-
+from ..backend.backend import State, Customer
+from sqlmodel import select  # Ensure this import is included
 
 def item(
-    country: str, initials: str, progress: int, color: LiteralAccentColor
+    mode: str, count: int, color: LiteralAccentColor
 ) -> rx.Component:
     return (
         rx.hstack(
             rx.hstack(
-                flag(initials),
                 rx.text(
-                    country,
+                    mode,
                     size="3",
                     weight="medium",
                     display=["none", "none", "none", "none", "flex"],
@@ -32,7 +21,7 @@ def item(
             ),
             rx.flex(
                 rx.text(
-                    f"{progress}%",
+                    f"{count}%",
                     position="absolute",
                     top="50%",
                     left=["90%", "90%", "90%", "90%", "95%"],
@@ -40,7 +29,7 @@ def item(
                     size="1",
                 ),
                 rx.progress(
-                    value=progress,
+                    value=int(count),
                     height="19px",
                     color_scheme=color,
                     width="100%",
@@ -55,14 +44,30 @@ def item(
         ),
     )
 
+def work_mode_chart() -> rx.Component:
+    with rx.session() as session:
+        customers = session.exec(select(Customer)).all()
 
-def adquisition() -> rx.Component:
+        mode_counts = {
+            "Remote": 0,
+            "Hybrid": 0,
+            "In Person": 0,
+            "Unknown": 0,
+        }
+
+        total = 0
+
+        for c in customers:
+            total += 1
+            mode = c.mode
+            if mode in mode_counts:
+                mode_counts[mode] += 1
+
     return rx.vstack(
-        item("USA", "US", 46, "blue"),
-        item("Canada", "CA", 33, "crimson"),
-        item("France", "FR", 28, "plum"),
-        item("Germany", "DE", 20, "green"),
-        item("Spain", "ES", 15, "amber"),
+        item("Remote", int(mode_counts["Remote"] / total* 100), "blue"),
+        item("Hybrid", int(mode_counts["Hybrid"] / total * 100), "crimson"),
+        item("In Person", int(mode_counts["In Person"] / total * 100), "plum"),
+        item("Unknown", int(mode_counts["Unknown"] / total * 100), "gray"),
         width="100%",
         spacing="6",
     )
